2c295b6911c507567854bb5634e4c1e55a78d17a
technzen <technzen1@gmail.com>
2024-05-23 14:55:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+//types of variables
+
+// number => 2^53 something range
+
+//bigint
+
+//string => ""
+
+//boolean => true/false
+
+//null => standalone value 
+
+//undefined =>
+
+// symbol => define uniqueness
+
+//object
+
+console.log(typeof null); //typeof null is object
+console.log(typeof undefined);
